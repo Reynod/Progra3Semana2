bf76d048aee284a20cbca19dca823c59fb5832d7
Reynod <reynod.bocanegra@gmail.com>
2017-04-27 10:55:37 -0500

    arreglos

---
+++ arreglos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+ int size=5;
+ int arreglo1[size];//lo más recomendado si puede variar el tamaño del arreglo.
+
+ int arreglo2[]={ 10,20,30,40,50};
+ char diasSemana[]={'L','M','m','J','V','S','D'};
+ 
+ //matrices 
+ int matriz[5][5];
+  
+ return 0;
+}
